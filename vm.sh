4d19209c774578a712fdc48ef447162917a3d0bf
Five <five@railway.app>
2023-03-26 14:19:23 +0800

    vm.sh: asia-southeast1-a

---
+++ vm.sh
@@ -0,0 +1,12 @@
+gcloud compute instances create wynyard-0 \
+  --enable-nested-virtualization \
+  --zone asia-southeast1-a \
+  --machine-type "n1-standard-32" \
+  --boot-disk-size "100GB" \
+  --image-family "debian-11" \
+  --image-project "debian-cloud"
+gcloud compute disks create wynyard-0-disk-0 \
+  --size 50 \
+  --type "pd-ssd" \
+  --zone asia-southeast1-a
+gcloud compute instances attach-disk wynyard-0 --disk wynyard-0-disk-0
