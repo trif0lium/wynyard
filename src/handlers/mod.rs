cc87d6b67cd95ca67278a236299bb5f484a90b95
Five <five@railway.app>
2023-03-27 14:15:05 +0800

    handlers: init_http() + list_volumes()

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,1 +1,13 @@
-mod handlers {}
+use axum::{
+    Router,
+    routing::get,
+    response::{Headers, IntoResponse},
+};
+
+pub mod handlers {
+    async fn init_http() {
+        let app = Router::new().route("/volumes", get(list_volumes));
+    }
+
+    async fn list_volumes() -> impl IntoResponse {}
+}
